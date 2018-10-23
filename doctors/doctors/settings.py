e63f0e0099f066b74d857ae4d42d2e0ce98c3ee5
PandorAstrum <primeintegerslab@gmail.com>
2018-10-24 01:44:37 +0600

    Spider Created and First Website coded

--- doctors/doctors/settings.py
+++ doctors/doctors/settings.py
@@ -19,30 +19,34 @@ NEWSPIDER_MODULE = 'doctors.spiders'
 #USER_AGENT = 'doctors (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+# ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+# CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 2
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
 #TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
-#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-#   'Accept-Language': 'en',
-#}
+DEFAULT_REQUEST_HEADERS = {
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'accept-encoding': 'gzip, deflate, sdch, br',
+    'accept-language': 'en-GB,en;q=0.8,en-US;q=0.6,ml;q=0.4',
+    'cache-control': 'max-age=0',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
+}
 
 # Enable or disable spider middlewares
 # See https://doc.scrapy.org/en/latest/topics/spider-middleware.html
@@ -88,3 +92,8 @@ ROBOTSTXT_OBEY = True
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+
+FEED_EXPORTERS = {
+    'csv': 'doctors.exporters.FixLineCsvItemExporter',
+}
--- doctors/doctors/spiders/md.py
+++ doctors/doctors/spiders/md.py
@@ -1,14 +1,72 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy import Request
+from scrapy.spiders import CrawlSpider, Rule, Spider
+from scrapy.linkextractors import LinkExtractor
+from lxml import html
 
+_list_of_page = ['https://www.md.com/doctors/allergist-immunologist/florida/~/~',
+                  'https://www.md.com/doctors/cardiologist/florida/~/~',
+                  'https://www.md.com/doctors/dermatologist/florida/~/~',
+                  'https://www.md.com/doctors/ent-otolaryngologist/florida/~/~',
+                  'https://www.md.com/doctors/family-doctor/florida/~/~']
 
-class MdSpider(scrapy.Spider):
+class MdSpider(Spider):
     name = 'md'
     allowed_domains = ['md.com']
-    start_urls = ['https://www.md.com/doctors/cardiologist/florida/~/~']
+    start_urls = ['https://www.md.com/doctors/family-doctor/florida/~/~']
+
     def start_requests(self):
-        # all urls here 
-        pass
+        for url in self.start_urls:
+            yield Request(url, callback=self.parse)
 
     def parse(self, response):
-        pass
+        all_urls = response.xpath('//a[@itemprop="url"]/@href').extract()
+        doctor_urls = list(set(all_urls))  # make unique
+        for doctor in doctor_urls:
+            yield Request(doctor, callback=self.parse_deeper)
+    #         # yield Request(doctor, self.parse_deeper)
+        next_page_url = response.xpath('//li[@class="pagination_next"]//a/@href').extract_first()
+        if next_page_url:
+            yield Request(next_page_url)
+
+    def parse_deeper(self, response):
+        doctor_name = response.xpath('//span[@itemprop="name"]/text()').extract_first()
+        speciality = response.xpath('//div[@class="breadcrumbs"]/div[3]/a/span/text()').extract_first()
+        first_office = response.xpath('//div[@itemprop="address"]')[0]
+        office_name = first_office.xpath('//h3[@itemprop="name"]/text()').extract_first().strip()
+        telephone = first_office.xpath('//span[@itemprop="telephone"]/text()').extract_first()
+        if telephone:
+            pass
+        else:
+            telephone = "No Phone"
+        address = first_office.xpath('//span[@itemprop="streetAddress"]')
+        address_1 = address.xpath('//span[@class="street-address-1"]/text()').extract_first()
+        address_2 = address.xpath('//span[@class="street-address-2"]/text()').extract_first()
+        if address_2:
+            final_street_address = address_1 + " " + address_2
+        else:
+            final_street_address = address_1
+        city = first_office.xpath('//span[@itemprop="addressLocality"]/text()').extract_first()
+        zip_code = first_office.xpath('//span[@itemprop="postalCode"]/text()').extract_first()
+        state = first_office.xpath('//span[@itemprop="addressRegion"]/text()').extract_first()
+        yield {
+            'Doctor Name' : doctor_name,
+            'Speciality' : speciality,
+            'Institute' : office_name,
+            'Street Address' : final_street_address,
+            'City' : city,
+            'State' : state,
+            'Zip' : zip_code,
+            'Phone Number' : telephone,
+            'URL': response.url,
+        }
+
+
+# from scrapy.crawler import CrawlerProcess
+#
+# c = CrawlerProcess({
+#     'USER_AGENT': 'Mozilla/5.0',
+# })
+# c.crawl(MdSpider)
+# c.start()
